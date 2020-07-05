5c7736aa995947f9863bb797078af21f51bc40d1
Parkour3007 <joshuashterenberg@gmail.com>
2020-07-05 18:45:00 -0500

    Going through the tutorial, trying to understand it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 
+//Functions only to point to external app so we don't have to mess with the built in stuff
 function App() {
   return (
     <div className="App">
